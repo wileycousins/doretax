ed3f76784b3793901b8a53476f07aeb7099577c7
Zack Dever <zack@decode72.com>
2011-09-25 11:28:48 -0500

    cole's static html in the django project now. first pass anyways.

--- doretax/views.py
+++ doretax/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render_to_response
+from doretax import settings
+ 
+common_args = {
+               'STATIC_URL' : settings.STATIC_URL,
+               } 
  
 def home(request):
-    return render_to_response('home.html')
+    return render_to_response('home.html', common_args)
     
 def about(request):
-    return render_to_response('about.html')
+    return render_to_response('about.html', common_args)
     
 def client_center(request):    
-    return render_to_response('client-center.html')
+    return render_to_response('client-center.html', common_args)
     
 def community(request):
-    return render_to_response('community.html')
+    return render_to_response('community.html', common_args)
     
 def contact(request):
-    return render_to_response('contact.html')
+    return render_to_response('contact.html', common_args)
