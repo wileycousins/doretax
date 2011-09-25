3c60aca6c99c54f506559e59294fc638c22c1ed9
Zack Dever <zack@decode72.com>
2011-09-25 16:49:39 -0500

    umkay, i think the views should be good now

--- doretax/urls.py
+++ doretax/urls.py
@@ -1,24 +1,18 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin 
+from django.views.defaults import page_not_found
 from django.views.generic.simple import redirect_to, direct_to_template
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^$', 'views.home'),
-    (r'^about$', 'views.about'),
-    (r'^client-center$', 'views.client_center'),
-    (r'^community$', 'views.community'),
-    (r'^contact$', 'views.contact'),
-    
     (r'^favicon.ico$', redirect_to, {'url': '/site_media/static/images/fav.ico'}),
-    (r'^robots.txt$', direct_to_template, {'template':'robots.txt', 'mimetype':'text/plain'}),
-    (r'^sitemap.txt$', direct_to_template, {'template':'sitemap.txt', 'mimetype':'text/plain'}),
     (r'^get/(?P<page>.*)$', 'views.get'),
-    # (r'^doretax/', include('doretax.foo.urls')),
-
     # Uncomment the admin/doc line below to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    (r'^$', 'views.default', {'page':'home'}),
+    (r'^home$', page_not_found),
+    (r'^robots.txt$', direct_to_template, {'template':'robots.txt', 'mimetype':'text/plain'}),
+    (r'^sitemap.txt$', direct_to_template, {'template':'sitemap.txt', 'mimetype':'text/plain'}),
     (r'^admin/', include(admin.site.urls)),
+    (r'^(?P<page>.*)$', 'views.default'), # make sure this is last as it will catch everything
 )
