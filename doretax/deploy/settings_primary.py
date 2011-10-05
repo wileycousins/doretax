5597c8aa5a8f6393494ec8f513d1f77458ac2634
Zack Dever <zack@decode72.com>
2011-10-04 19:39:30 -0500

    maybe the port number is to be a string?

--- doretax/deploy/settings_primary.py
+++ doretax/deploy/settings_primary.py
@@ -1,7 +1,7 @@
 SEND_BROKEN_LINK_EMAILS = True
 
 EMAIL_HOST = "smtp.google.com"
-EMAIL_PORT = 587
+EMAIL_PORT = "587"
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "HAL@decode72.com"
 EMAIL_HOST_PASSWORD = "geauxmice"
