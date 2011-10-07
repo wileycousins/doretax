d1fa987b15862a4dd16050dc96742ddbf9619a80
Zack Dever <zack@decode72.com>
2011-10-06 23:00:24 -0500

    some left over settings changes. stupid email.

--- doretax/settings.py
+++ doretax/settings.py
@@ -6,21 +6,6 @@ DEBUG = True
 #IS_DEV = True
 TEMPLATE_DEBUG = DEBUG
 
-SEND_BROKEN_LINK_EMAILS = True
-
-EMAIL_HOST = "smtp.google.com"
-EMAIL_PORT = "587"
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "HAL@decode72.com"
-EMAIL_HOST_PASSWORD = "geauxmice"
-
-#EMAIL_HOST = 'mail6.webfaction.com'
-#EMAIL_HOST_USER = 'wd40too'
-#EMAIL_HOST_PASSWORD = 'geaux44'
-#EMAIL_PORT = '25'
-DEFAULT_FROM_EMAIL = "HAL@decode72.com"
-SERVER_EMAIL = "HAL@decode72.com"
-
 # root directories
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
