f6a6bd7c86b6fabe520466527a2912acfae563f8
Zack Dever <zack@decode72.com>
2012-01-22 16:26:18 -0600

    trying out new contact email on the contact form

--- doretax/deploy/settings_primary.py
+++ doretax/deploy/settings_primary.py
@@ -3,6 +3,6 @@ SEND_BROKEN_LINK_EMAILS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "support@decode72.com"
+EMAIL_HOST_USER = "contact@doretax.com"
 EMAIL_HOST_PASSWORD = "geauxmice"
 
